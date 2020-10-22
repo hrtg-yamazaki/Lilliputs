14a5eae18da1f2f02b68c573483116017f1607d5
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-10-22 20:24:41 +0900

    リスト表示のレイアウト編集
    ・前回作成したリスト表示機能に関するレイアウトを編集しました。

--- code/resources/views/recipes/index.blade.php
+++ code/resources/views/recipes/index.blade.php
@@ -16,24 +16,31 @@
 
 @section('content')
 
-    <div class="content">
-        <div class="content__box">
+    <div class="index-sections">
 
-            <ul>
-                @foreach($recipes as $recipe)
-                    <li>
-                        <a href={{ route('recipes.show', ['recipe' => $recipe]) }}>
-                            {{ $recipe->title }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-
-            @auth
-                <p>{{ $user->name }}</p>
-            @endauth
-
+        <div class="index-section">
+            <div class="index-section__box">
+                <h1 class="index-section__box__head">
+                    最新の投稿
+                </h1>
+                <div class="index-section__box__content">
+                    <ul class="recipe-list">
+                        @foreach($recipes as $recipe)
+                            <li class="recipe-panel">
+                                <a href={{ route('recipes.show', ['recipe' => $recipe]) }} class="recipe-panel__inner">
+                                    {{ $recipe->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
+
+        @auth
+            <p>{{ $user->name }}</p>
+        @endauth
+
     </div>
 
 @endsection
