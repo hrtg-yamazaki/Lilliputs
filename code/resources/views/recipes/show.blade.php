3083e0f4918a7738188f402b3fea76f798e548ee
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-10-24 17:26:50 +0900

    詳細ページのレイアウト編集
    ・show.blade.phpを編集し、レシピの詳細情報が表示できるようにしました。
    ・_show.scssを作成し、詳細ページの上部レイアウトに関する記述を追加しました。

--- code/resources/views/recipes/show.blade.php
+++ code/resources/views/recipes/show.blade.php
@@ -3,14 +3,33 @@
 
 @section('content')
 
-    <div class="content">
-        <div class="content__box">
+    <div class="show-sections">
 
-            <h1>{{ $recipe->title }}</h1>
-            <br>
-            <p>{{ $recipe->description }}</p>
-
+        <div class="show-section">
+            <div class="show-section__box">
+                <div class="show-head clearfix">
+                    <div class="show-head__left">
+                        <div class="show-no-image">
+                            No image
+                        </div>
+                    </div>
+                    <div class="show-head__right">
+                        <h2 class="show-title">
+                            {{ $recipe->title }}
+                        </h2>
+                        <p class="show-category">
+                            Maingred × Method
+                        </p>
+                        <p class="half-border">&nbsp;</p>
+                        <p class="show-description">
+                            &nbsp;&nbsp;{{ $recipe->description }}
+                        </p>
+                        <p class="half-border">&nbsp;</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 
 @endsection
