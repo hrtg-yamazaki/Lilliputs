66d9502a92b9815d841b4afc9ad1f470650eeb8e
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-10-30 22:53:33 +0900

    SSL証明書関連の再設定
    ・SSL証明書関連のリソースと、httpsでの接続、httpからhttpsへのredirectに関連する設定を行いました。

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -12,6 +12,13 @@ resource "aws_security_group" "lilliputs_alb" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
