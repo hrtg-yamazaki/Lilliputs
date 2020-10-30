2b056d65dccd9797399fa0586e94831306da3571
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-10-30 18:15:31 +0900

    asset読み込みの条件分岐文解除
    ・本番環境でコンテナが落ちる問題の原因と考えられる記述を元に戻しました。

--- code/resources/views/layout.blade.php
+++ code/resources/views/layout.blade.php
@@ -5,13 +5,8 @@
         <title>{{ env('APP_NAME', 'laravel')}}</title>
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <meta name='csrf-token' content='{{ csrf_token() }}'>
-        @if(App::environment("production"))
-            <link rel='stylesheet' href='{{ secure_asset("css/app.css") }}'>
-            <script src='{{ secure_asset("js/app.js") }}' defer></script>
-        @else
-            <link rel='stylesheet' href='{{ asset("css/app.css") }}'>
-            <script src='{{ asset("js/app.js") }}' defer></script>
-        @endif
+        <link rel='stylesheet' href='{{ asset("css/app.css") }}'>
+        <script src='{{ asset("js/app.js") }}' defer></script>
     </head>
     <body>
 
