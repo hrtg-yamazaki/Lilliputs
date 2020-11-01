734ff1bd2073c729f971368d1415d8f982be9c37
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-11-01 13:42:30 +0900

    検索TOPページのレイアウト調整
    ・recipes/search/top.blade.phpとrecipes/_search.scssを編集し、検索TOPページのレイアウトを調整しました。

--- code/resources/views/recipes/search/top.blade.php
+++ code/resources/views/recipes/search/top.blade.php
@@ -1,10 +1,31 @@
-@extends('layout')
+@extends('single_layout')
 
 
 @section('content')
 
-    <h1>
-        {{ $message }}
-    </h1>
+    <div class="search-contents">
+        <div class="search-contents__box">
+            <h2 class="search-contents__box__head">
+                検索
+            </h2>
+            <div class="search-contents__box__content">
+                <p class="search-message">
+                    検索方法を選択してください
+                </p>
+                <ul class="search-ways">
+                    <li class="search-way">
+                        <a href="#" class="search-way__link">
+                            タイトルで検索
+                        </a>
+                    </li>
+                    <li class="search-way">
+                        <a href="#" class="search-way__link">
+                            メイン食材・調理方法で検索
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 @endsection
