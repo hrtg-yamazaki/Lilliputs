5cf3885ad5c128be91654d1726c0c9b154ca6317
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-11-04 19:47:43 +0900

    詳細ページのレスポンシブ対応
    ・_responsive.scssを編集し、recipes.showページのレイアウトがスマートフォンやタブレットでも崩れないよう、CSSの設定を追加しました。

--- code/resources/views/shared/ingredient.blade.php
+++ code/resources/views/shared/ingredient.blade.php
@@ -3,8 +3,8 @@
     <div class="ingredient__box clearfix">
         <p class="ingredient__box__name">
             {{ $ingredient->name }}
-        </p>
-        <p class="ingredient__box__amount">
+        </p>{{--
+    --}}<p class="ingredient__box__amount">
             {{ $ingredient->amount }}
         </p>
     </div>
