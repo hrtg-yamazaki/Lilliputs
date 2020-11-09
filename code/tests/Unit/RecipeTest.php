6055b45eafa0d1571b66c8d88ad324f49b54a384
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-11-09 21:07:50 +0900

    Recipeモデルの単体テスト(外部キー)
    ・Recipeにuser_id, maingred_id, method_idを追加し数字であれば登録できることを確認するテストを行いました。

--- code/tests/Unit/RecipeTest.php
+++ code/tests/Unit/RecipeTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Database\QueryException;
 use App\Recipe;
+use App\User;
 
 class RecipeTest extends TestCase
 {
@@ -70,6 +71,42 @@ class RecipeTest extends TestCase
         $recipe->description = null;
 
         $recipe->save();
+    }
+
+    /**
+     * user_idが数字であれば登録できる
+     */
+    public function testCreateRecipeWithUserId(){
+        $recipe = self::createSampleRecipe();
+
+        $recipe->user_id = 1;
+        $recipe->save();
+
+        $this->assertTrue(isset($recipe->id));
+    }
+
+    /**
+     * maingred_idが数字であれば登録できる
+     */
+    public function testCreateRecipeWithMaingredId(){
+        $recipe = self::createSampleRecipe();
+
+        $recipe->maingred_id = 1;
+        $recipe->save();
+
+        $this->assertTrue(isset($recipe->id));
+    }
+
+    /**
+     * method_idが数字であれば登録できる
+     */
+    public function testCreateRecipeWithMethodId(){
+        $recipe = self::createSampleRecipe();
+
+        $recipe->method_id = 1;
+        $recipe->save();
+
+        $this->assertTrue(isset($recipe->id));
     }
 
     /**
