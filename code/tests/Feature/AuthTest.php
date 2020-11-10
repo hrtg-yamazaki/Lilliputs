027fdc1824d6538cab4b09206d32d4b99ff0a406
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-11-10 20:04:35 +0900

    新規登録関連のテスト
    ・Feature/AuthTestを編集し、新規登録に関連するテストを行いました。

--- code/tests/Feature/AuthTest.php
+++ code/tests/Feature/AuthTest.php
@@ -59,6 +59,94 @@ class AuthTest extends TestCase
         $this->assertGuest();
     }
 
+    /**
+     * 新規登録ページへのアクセス
+     */
+    public function testAccessRegister()
+    {
+        $response = $this->get("/register");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * 新規登録のテスト(DB、送信後リダイレクト)
+     */
+    public function testRegister()
+    {
+        $formData = [
+            "name"=>"サンプルユーザー２",
+            "email"=>"sample2@sample.com",
+            "password"=>"samplepassword2",
+            "password_confirmation"=>"samplepassword2"
+        ];
+        $response = $this->from("register")
+                        ->post("register", $formData);
+
+        $this->assertDatabaseHas("users", ["name"=>"サンプルユーザー２"]);
+        $response->assertRedirect("/");
+    }
+
+    /**
+     * 不足する要素があると新規登録できず再表示される
+     */
+    // name
+    public function testFailToRegisterWithoutName()
+    {
+        $formData = [
+            "email"=>"sample3@sample.com",
+            "password"=>"samplepassword3",
+            "password_confirmation"=>"samplepassword3"
+        ];
+        $response = $this->from("register")
+                        ->post("register", $formData);
+
+        $response->assertRedirect("/register");
+    }
+    // email
+    public function testFailToRegisterWithoutEmail()
+    {
+        $formData = [
+            "name"=>"サンプルユーザー３",
+            "password"=>"samplepassword3",
+            "password_confirmation"=>"samplepassword3"
+        ];
+        $response = $this->from("register")
+                        ->post("register", $formData);
+
+        $response->assertRedirect("/register");
+    }
+    // password
+    public function testFailToRegisterWithoutPassword()
+    {
+        $formData = [
+            "name"=>"サンプルユーザー３",
+            "email"=>"sample3@sample.com",
+            "password_confirmation"=>"samplepassword3"
+        ];
+        $response = $this->from("register")
+                        ->post("register", $formData);
+
+        $response->assertRedirect("/register");
+    }
+    // password_confirmation
+    public function testFailToRegisterWithoutPasswordConfirmation()
+    {
+        $formData = [
+            "name"=>"サンプルユーザー３",
+            "email"=>"sample3@sample.com",
+            "password"=>"samplepassword3",
+        ];
+        $response = $this->from("register")
+                        ->post("register", $formData);
+
+        $response->assertRedirect("/register");
+    }
+
+
+    /**
+     * private
+     */
 
     /**
      * サンプルユーザーの作成
