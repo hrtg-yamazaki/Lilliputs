2b9e94954625f0f07d04267bec7c84ab55aef7a1
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-11-18 16:30:06 +0900

    メール送信ページのレイアウト調整
    ・email.blade.phpを編集し、パスワードリセットに関するメール送信ページのレイアウトを調整しました。

--- code/resources/views/auth/passwords/email.blade.php
+++ code/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,39 @@
-@extends('layout')
+@extends('single_layout')
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="auth-section">
+        <div class="auth-form">
+            <form method="POST" action="{{ route('password.update') }}">
+                @csrf
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                <h2 class="auth-form__box__head">
+                    パスワード再登録
+                </h2>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="auth-form__box__content">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="auth-field">
+                        <label for="email" class="auth-field__label">メールアドレス</label>
+                        <input id="email" type="email" class="auth-field__text @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <div class="auth-submit">
+                        <button type="submit" class="auth-submit__button">
+                            メールを送信する
+                        </button>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
-            </div>
+
+            </form>
         </div>
     </div>
-</div>
+
 @endsection
